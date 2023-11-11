f68db9dfd017fd3f819445a3a2bb35ab90a9a4a1
JuaniPardo <juani.pardo@icloud.com>
2023-11-11 15:22:27 -0300

    merge de product detail con main

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -4,7 +4,8 @@
     @component('components.navbar')
     @endcomponent
     <!-- Breadcrumb -->
-    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+    <nav class="bg-th-info"
+        style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
         <div class="container py-2">
             <ol class="breadcrumb mb-0">
                 <li class="breadcrumb-item"><a href="{{ route('home') }}" class="text-white-50">Home</a></li>
