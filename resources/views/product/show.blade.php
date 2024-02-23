b9f0e8bd023b83efa459f9d3193942e313236bd7
JuaniPardo <juani.pardo@icloud.com>
2024-02-23 00:21:34 -0300

    Se corrigio imagenes hardcodeadas en la vista de producto

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -30,67 +30,28 @@
         <div class="container py-5">
             <div class="row gx-5 justify-content-around">
                 <div class="col-lg-6">
-                    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
-                        <!-- Carousel Slides -->
-                        <div class="carousel-inner border rounded-4">
-                            <!-- Slide 1 -->
-                            <div class="carousel-item active img-container">
-                                <div data-bs-toggle="lightbox" data-bs-slide-to="0" class="">
-                                    <img src="https://redragon.es/content/uploads/2021/04/CETROPHORUS-RGB.png"
-                                         class="img-fluid img-cover rounded mx-auto" alt="Image 1">
-                                </div>
-                            </div>
-                            <!-- Slide 2 -->
-                            <div class="carousel-item img-container">
-                                <div data-bs-toggle="lightbox" data-bs-slide-to="1">
-                                    <img
-                                        src="https://www.infinitonline.com.ar/images/000000000000128288000aquila-fly-black-matte-11-d5e03a8e6b5b7f2c0f16527437443635-1024-1024--1-.png"
-                                        class="img-fluid img-cover rounded mx-auto" alt="Image 2">
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Carousel Slides -->
 
-                        <!-- Thumbnails -->
-                        <div class="d-flex justify-content-center mt-3">
-                            <!-- Thumbnail 1 -->
-                            <a data-bs-target="#carouselExample" data-bs-slide-to="0" class="border mx-1 rounded-2"
-                               href="#">
-                                <img width="60" height="60" class="rounded-2"
-                                     src="https://redragon.es/content/uploads/2021/04/CETROPHORUS-RGB.png"
-                                     alt="Thumbnail 1">
-                            </a>
-                            <!-- Thumbnail 2 -->
-                            <a data-bs-target="#carouselExample" data-bs-slide-to="1" class="border mx-1 rounded-2"
-                               href="#">
-                                <img width="60" height="60" class="rounded-2"
-                                     src="https://www.infinitonline.com.ar/images/000000000000128288000aquila-fly-black-matte-11-d5e03a8e6b5b7f2c0f16527437443635-1024-1024--1-.png"
-                                     alt="Thumbnail 2">
-                            </a>
-                        </div>
-                        <!-- Thumbnails -->
-                    </div>
                     {{--Version con imagenes en storage--}}
-                    {{--<div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+                    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-inner bg-th-info rounded-2">
-                            @foreach($imagenes as $imagen)
+                            @foreach($product->images as $image)
                                 <div class="carousel-item{{ $loop->first ? ' active' : '' }} img-container">
                                     <div data-bs-toggle="lightbox" data-bs-slide-to="{{ $loop->index }}">
-                                        <img src="{{ $imagen->ruta }}" class="img-fluid img-cover rounded mx-auto" alt="Image {{ $loop->iteration }}">
+                                        <img src="{{ asset($image->url) }}" class="img-fluid img-cover rounded mx-auto" alt="Image {{ $loop->iteration }}">
                                     </div>
                                 </div>
                             @endforeach
                         </div>
                         <!-- Thumbnails -->
                         <div class="d-flex justify-content-center mt-3">
-                            @foreach($imagenes as $imagen)
+                            @foreach($product->images as $image)
                                 <a data-bs-target="#carouselExample" data-bs-slide-to="{{ $loop->index }}" class="border mx-1 rounded-2" href="#">
-                                    <img width="60" height="60" class="rounded-2" src="{{ $imagen->ruta }}" alt="Thumbnail {{ $loop->iteration }}">
+                                    <img width="60" height="60" class="rounded-2" src="{{ asset($image->url) }}" alt="Thumbnail {{ $loop->iteration }}">
                                 </a>
                             @endforeach
                         </div>
                         <!-- Thumbnails -->
-                    </div>--}}
+                    </div>
 
 
                 </div>
@@ -100,18 +61,7 @@
                             {{ $product->name }}
                         </h4>
                         <div class="d-flex flex-row my-3">
-                            {{--<div class="text-warning mb-1 me-2">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fas fa-star-half-alt"></i>
-                                <span class="ms-1">
-                                    4.5
-                                </span>
-                            </div>
-                            <span class="text-muted"><i class="fas fa-shopping-basket fa-sm mx-1"></i>154 orders</span>
-                            <span class="text-success ms-2">In stock</span>--}}
+
                         </div>
 
                         <div class="mb-3">
@@ -122,20 +72,6 @@
                         <p>
                             {{ $product->long_desc }}
                         </p>
-
-                        {{--<div class="row">
-                            <dt class="col-3">Type:</dt>
-                            <dd class="col-9">Regular</dd>
-
-                            <dt class="col-3">Color</dt>
-                            <dd class="col-9">Brown</dd>
-
-                            <dt class="col-3">Material</dt>
-                            <dd class="col-9">Cotton, Jeans</dd>
-
-                            <dt class="col-3">Brand</dt>
-                            <dd class="col-9">Reebook</dd>
-                        </div>--}}
 
                         <hr/>
                         <form action="{{ route('shoppingCart.addProduct') }}" method="POST" class="">
@@ -163,15 +99,13 @@
                                     </div>
                                 </div>
                             </div>
-                            <a href="#" class="btn btn-warning shadow-0 disabled"> Buy now </a>
-                            {{--                        <form action="{{ route('shoppingCart.addProduct') }}" method="POST" class="d-inline">--}}
+{{--                            <a href="#" class="btn btn-warning shadow-0 disabled"> Buy now </a>--}}
                             <input type="hidden" name="product_id" value="{{ $product->id }}">
                             <button type="submit" class="btn btn-primary"><i class="me-1 fa fa-shopping-basket"></i> Add
                                 to cart
                             </button>
                         </form>
-                        {{--<a href="#" class="btn btn-light border border-secondary py-2 icon-hover px-3"> <i
-                                class="me-1 fa fa-heart fa-lg"></i> Save </a>--}}
+
                     </div>
                 </main>
             </div>
